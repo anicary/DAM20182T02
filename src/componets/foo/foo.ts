6d6ea1b10ba48a2dfe576ae98b9619ff581aaac6
ancamora <ancamondragonra@ittepic.edu.mx>
2018-10-10 15:32:28 -0600

    t03

--- src/componets/foo/foo.ts
+++ src/componets/foo/foo.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 @Component({
- selector: 'my-component',
- templateUrl: 'foo.html'
+    selector: 'my-component',
+    templateUrl: 'foo.html'
 })
 export class MyComponent {
- public data: any = {myToggle: true};
- constructor() {}
- isClicked(val) {
- console.log('Vegetariano: ' + val);
+    public data: any = {myToggle: true};
+    constructor() {}
+    isClicked(val) {
+    console.log('Vegetariano: ' + val);
  }
 }
